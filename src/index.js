ee8fa7abb13e58e1502ae58d99558bb141e46cf0
Lebokgane <lebokgane1@gmail.com>
2025-10-23 22:55:16 +0200

    Added basic search engine without API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-alert("hello world");
+function handleSearchSubmit(event) {
+event.preventDefault();
+let searchInput = document.querySelector("#search-form-input");
+let weatherAppCityElement = document.querySelector("#weather-app-city");
+weatherAppCityElement.innerHTML = searchInput.value;
+}
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
